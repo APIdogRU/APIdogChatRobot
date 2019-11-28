bf069d90fb457373f2edc4ea1f1a0db486ea936b
Vladislav Veluga <vladislav805@yandex.com>
2019-11-28 23:22:12 +0300

    jokes: рефактор и очистка лишнего

--- src/jokes.ts
+++ src/jokes.ts
@@ -1,7 +1,10 @@
 import * as TelegramBot from 'node-telegram-bot-api';
-import { DAY, getRandomInt, HOUR } from './utils';
+import { DAY, getNow, getRandomInt, HOUR } from './utils';
 import { Reply } from './reply';
 import { IReasonJoke } from './interfaces';
+import { makeKarmaTransaction } from './karma';
+import { toStringDateTime } from './time';
+import * as Sugar from 'sugar';
 
 /***********************************************
  * Проверка и ответ на сообщения-команды-шутки *
@@ -23,20 +26,37 @@ const jokes: IJoke[] = [
 	{ command: '/praygnu', delta: 42, coolDown: () => 30 + getRandomInt(7 * DAY - 30), reason: 'praygnu' }
 ];
 
+const coolDownInfo: Record<string, number> = {};
+
 export default (message: TelegramBot.Message, reply: () => Reply) => {
 
 	const ent = message.entities[0];
 	const substr = message.text.substr(ent.offset, ent.length);
 
+	const make = async (joke: IJoke) => {
+		const now = getNow();
+		const user = message.from;
+		const coolDown = Sugar.Object.isFunction(joke.coolDown) ? joke.coolDown() : joke.coolDown;
+		const key = user.id + '_' + joke.command.substring(1);
+
+		if (coolDownInfo[key] + coolDown > now) {
+			reply().text(`Рано. Осталось ${toStringDateTime(coolDownInfo[user.id] + coolDown - now)}`).asReply().send();
+			return;
+		}
+
+		coolDownInfo[key] = now;
+
+		const delta = Sugar.Object.isFunction(joke.delta) ? joke.delta() : joke.delta;
+		const [{ karma: karmaNew }] = await makeKarmaTransaction(message.from.id, delta);
+
+		reply().text(`<b>👤 ${user.username || user.first_name}</b>\n🔺 Карма: <code>+${delta}</code> -> <code>${karmaNew}</code>\n▫️ Кулдаун: ${toStringDateTime(coolDown)}`).asReply().send();
+	};
+
 	jokes.some(joke => {
 		if (joke.command === substr) {
-			let str = ['Joke'];
 
-			str.push(`name = ${joke.command}`);
-			str.push(`delta = ${joke.delta}`);
-			str.push(`colldown = ${joke.coolDown}`);
+			make(joke);
 
-			reply().text(str.join('\n')).send();
 			return true;
 		}
 	});
