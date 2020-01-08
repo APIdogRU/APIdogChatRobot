47929eb825fe7cf98969543fab9bcc4e2f17ec93
Vladislav Veluga <vladislav805@yandex.com>
2020-01-08 23:42:29 +0300

    Неработающая бета

--- src/index.ts
+++ src/index.ts
@@ -10,6 +10,7 @@ import banInit from './bans';
 import usersInit from './users';
 import registerScreenshoter from './screenshot';
 import { getRandomInt } from './utils';
+import * as http from 'http';
 
 bot.onText(/\/test/i, (msg: TelegramBot.Message) => {
 	reply(bot, msg).text('Up').asReply().send();
@@ -21,6 +22,40 @@ bot.onText(/\/ping/i, (msg: TelegramBot.Message) => {
 
 bot.onText(/\/roll/i, (msg: TelegramBot.Message) => {
 	reply(bot, msg).text(['ОРЁЛ', 'РЕШКА'][getRandomInt(1)]).send();
+});
+
+bot.onText(/\/red/i, (msg: TelegramBot.Message) => {
+	if (!msg.reply_to_message) {
+		reply(bot, msg).text('Нет ответа').asReply().send();
+		return;
+	}
+
+	const url = msg.reply_to_message.text;
+
+	require('https').get(url, (res: http.IncomingMessage) => {
+		let data = '';
+		res.on('data', (chunk: string) => {
+			data += chunk;
+		});
+
+		res.on('end', () => {
+			const res = /<meta property="og:image" content="([^"]+)"\/>/ig.exec(data);
+
+			const image = res[1]?.replace(/&amp;/ig, '&');
+
+			if (!image) {
+				reply(bot, msg).text('Пикчу не нашёл').asReply().send();
+				return;
+			}
+
+			bot.sendPhoto(msg.chat.id, image, {
+				reply_to_message_id: msg.message_id,
+				disable_notification: true
+			});
+		});
+	}).on('error', (e: Error) => {
+		reply(bot, msg).text(`error: ${e.toString()}`).asReply().send();
+	});
 });
 
 raveInit(bot);
